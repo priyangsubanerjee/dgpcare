208ec63b7b2a201ad46898fd487084fa41254fef
Priyangsu Banerjee <priyangsu26@gmail.com>
2021-12-18 22:55:02 +0530

    D

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,6 +11,9 @@ class MyDocument extends Document {
       <Html lang="en" translate='no'>
         <Head>
             <meta name="google" content="notranslate" />
+            <meta property="og:title" content="Dgp covid resources | Careplus" />
+            <meta property="og:description" content="All necessary C-19 resosurces now at your finger tips" />
+            <meta property="og:image" content="https://ahrefs.com/blog/wp-content/uploads/2019/12/fb-how-to-become-an-seo-expert.png" />
         </Head>
         <body>
           <Main />
