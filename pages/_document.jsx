08189081fd30e3d9a338409caea62fae5ebab312
Priyangsu Banerjee <priyangsu26@gmail.com>
2021-12-18 23:18:40 +0530

    d

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,11 +11,15 @@ class MyDocument extends Document {
       <Html lang="en" translate='no'>
         <Head>
             <meta name="google" content="notranslate" />
+            <meta property="og:site_name" content="Care Plus"></meta>
             <meta property="og:url" content="https:careplus.vercel.app" />
-            <meta property="og:type" content="article" />
             <meta property="og:title" content="Dgp covid resources | Careplus" />
             <meta property="og:description" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, et." />
             <meta property="og:image" content="https://thumbs.dreamstime.com/b/simple-coronavirus-covid-icon-vector-illustration-your-projects-176237979.jpg" />
+            <meta property="og:type" content="website" />
+            <meta property="og:image:type" content="image/jpeg"/>
+            <meta property="og:image:width" content="300"/>
+            <meta property="og:image:height" content="300"/>
         </Head>
         <body>
           <Main />
