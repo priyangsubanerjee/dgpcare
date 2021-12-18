79fb637c8748f743e0fec484b02cc28dfefbff6c
Priyangsu Banerjee <priyangsu26@gmail.com>
2021-12-18 18:44:01 +0530

    d'

--- components/CategoryCard.jsx
+++ components/CategoryCard.jsx
@@ -1,5 +1,5 @@
 import styles from '../styles/CategoryCard.module.css'
-import * as BiICons from 'react-icons/bi'
+import * as BsICons from 'react-icons/bs'
 import * as MdIcons from 'react-icons/md'
 import * as IoIcons from 'react-icons/io'
 import Link from 'next/link'
@@ -9,7 +9,7 @@ function CategoryCard({props}) {
 
     return (
         <div className={styles.category_card}>
-                <MdIcons.MdOutlineHealthAndSafety className={styles.card_icon}/>
+                <BsICons.BsJournalMedical className={styles.card_icon}/>
                 <span className={styles.card_title}>{props.title}</span>
                 <p className={styles.card_body}>{props.description}</p>
                 <Link href={`category/` + props.slug}>
