3ffdf3ef3a6347212b8322ebe9780d4449dc48c7
Priyangsu Banerjee <priyangsu26@gmail.com>
2021-12-18 18:05:48 +0530

    d

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import {gql, GraphQLClient} from 'graphql-request'
 import styles from '../styles/Home.module.css'
 import * as BiICons from 'react-icons/bi'
+import data from '../database/data'
 import CategoryCard from '../components/CategoryCard.jsx'
 import * as HiIcons from 'react-icons/hi'
 import * as BsIcons from 'react-icons/bs'
@@ -16,7 +17,7 @@ export async function getStaticProps() {
         { 
             headers: { 
                 
-                'Authorization': process.env.GRAPH_CMS_TOKEN, 
+                'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2Mzk4MTQ0NzUsImF1ZCI6WyJodHRwczovL2FwaS1hcC1zb3V0aC0xLmdyYXBoY21zLmNvbS92Mi9ja3hiaXNsbmkzNGFjMDF4aTZyY2piNDJyL21hc3RlciIsImh0dHBzOi8vbWFuYWdlbWVudC1uZXh0LmdyYXBoY21zLmNvbSJdLCJpc3MiOiJodHRwczovL21hbmFnZW1lbnQuZ3JhcGhjbXMuY29tLyIsInN1YiI6ImQ2YTQ2MTkwLTg2OTktNDYyMS05OGU4LWQ0MTZkZjEwMTVmNyIsImp0aSI6ImNreGJqNzdlMjM1YTYwMXpjNmxyZzF1YzAifQ.yWn6v9potNWUXg8pt-_gC66V1QSgKzJFVlcwx5-H6ff_WbXxeBiGuNhlFvuHTX5sSnn8pZtRjfNEOFG4fVVfjjGA4wOX40NdPf3fcXwHSAnSnk6VEjdai_ucuRZChatNnRw7y8g1cNQaS7GoHfSZjYpW2cTCHFf6ljwDEgjZSTPl0d3KeQkNYc6v4qdtuHhFwEo0eJYDAc8_XngfLhw1iuf6OVnn6ZfnZ5_OJv_GjtbUcFRAWvDRUIdqQ8N8p3VMdwHx5sit8NcWXaDXr9l8fVeJVJuKqIn0kuB5ol-d4AjuS2vFe7Xj7xGa6Jt9mmCcZFpIy5-9_o7Z43YHH3JkdbCUMgcKsAto1Z2MPhM2XC8Yh_V3lJ3fMiiWgNcSEw5u-WN0Esvc5UP8n4Q5IK0e1F-ZgrDofPZWPUOr1RAJRdVPbAutCLOzE0SmKzIxZMqy0j1HF49SVke2uTeUwzR9r2iafXig3msoFOKxRtU5EDOryUikUlzLndiEl5rmmAlqA6Oi7i6rzttZgpR5dUOnr5k4OBVDPYXoIxIfiUiVFhwdXao4ARSQTrXKN-pNW1VE3DcRsFVEu0yo-wLffjA7f0WMlb-HIQKbF7W2q3r7UpvIX-326CeBa-kGFX2wPMBciI6gCdTXKbzlz0lnkjwtRenHq38W9O6eISXlz3oxvo0' 
             } 
     })
 
@@ -51,7 +52,7 @@ export default function Home({categories}) {
 
     console.log(categories)
     const [query, setQuery] = useState('')
-    const [data, setData] = useState(categories)
+    const [cData, setData] = useState(data)
 
     useEffect(() => {
         
@@ -66,7 +67,7 @@ export default function Home({categories}) {
         }
         else {
 
-            setData(categories)
+            setData(data)
 
         }
         
@@ -98,7 +99,7 @@ export default function Home({categories}) {
             <div className={styles.categories_grid}>
                 
                 {
-                    data.map((item, index) => {
+                    cData.map((item, index) => {
 
                         console.log(item.id)
 
