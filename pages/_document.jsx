b8de0e9086608807ebe58837dcee5628f0cef0c1
Priyangsu Banerjee <priyangsu26@gmail.com>
2021-12-18 23:27:21 +0530

    master

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,22 +11,13 @@ class MyDocument extends Document {
       <Html lang="en" translate='no'>
         <Head>
             <meta name="google" content="notranslate" />
-            {/* <meta property="og:site_name" content="Care Plus"></meta>
+            <meta property="og:site_name" content="Care Plus"></meta>
             <meta property="og:url" content="https:careplus.vercel.app" />
             <meta property="og:title" content="Dgp covid resources | Careplus" />
             <meta property="og:description" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, et." />
             <meta property="og:image" content="https://thumbs.dreamstime.com/b/simple-coronavirus-covid-icon-vector-illustration-your-projects-176237979.jpg" />
             <meta property="og:type" content="website" />
-            <meta property="og:image:type" content="image/jpeg"/>
-            <meta property="og:image:width" content="300"/>
-            <meta property="og:image:height" content="300"/> */}
-
-            <meta property="og:title" content="European Travel Destinations"/>
-            <meta property="og:type" content="article" />
-            <meta property="og:description" content="Offering tour packages for individuals or groups."/>
-            <meta property="og:image" content="http://euro-travel-example.com/thumbnail.jpg"/>
-            <meta property="og:url" content="http://euro-travel-example.com/index.htm"/>
-            <meta name="twitter:card" content="summary_large_image"/>
+            <meta property="og:image:type" content="image/jpg"/>
         </Head>
         <body>
           <Main />
