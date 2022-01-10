336da5378789064082d869178efde0c944f01653
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-01-10 11:31:38 +0530

    D

--- database/Links.js
+++ database/Links.js
@@ -4,7 +4,14 @@ const links = [
         title : "Instagram",
         description : "Make sure you follow our instagram page to get latest notifications !",
         link : "https://instagram.com/dgp_covid_resource",
-    }
+    },
+
+    {
+        title : "C-19 Resources",
+        description : "Get detailed report on nearest hospitals, ambulance services, oxygen suppliers, RT PCR, and many more.",
+        link : "/",
+    },
+
 ]
 
 
