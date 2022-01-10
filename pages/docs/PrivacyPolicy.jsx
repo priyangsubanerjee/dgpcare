4c359dab1d698caf4498ec2898deb31e1de0a5c8
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-01-10 16:56:31 +0530

    D

--- pages/docs/PrivacyPolicy.jsx
+++ pages/docs/PrivacyPolicy.jsx
@@ -17,12 +17,12 @@ function TermsandConditions() {
             <div className={styles.hr}></div>
 
             <div>
-                <h1>Privacy Policy</h1>
+                <h2>Privacy Policy</h2>
                 <p>Last updated: December 23, 2021</p>
                 <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
                 <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the <a href="https://www.privacypolicies.com/blog/privacy-policy-template/"  rel="noreferrer" target="_blank">Privacy Policy Template</a>.</p>
-                <h1>Interpretation and Definitions</h1>
-                <h2>Interpretation</h2>
+                <h2>Interpretation and Definitions</h2>
+                <h3>Interpretation</h3>
                 <p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
                 
             </div>
