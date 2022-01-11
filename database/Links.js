acc4bd9a101cb44fb3f75d2807b29fca89882e78
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-01-11 23:38:48 +0530

    Links updated

--- database/Links.js
+++ database/Links.js
@@ -1,11 +1,5 @@
 const links = [
 
-    {
-        title : "Instagram",
-        description : "Make sure you follow our instagram page to get latest notifications !",
-        link : "https://instagram.com/dgp_covid_resource",
-        target: '_blank',
-    },
 
     {
         title : "C-19 Resources",
